b89e3368774c13bf93e5020fb696ceaead24c18e
Alfarezi <alfareziy9@gmail.com>
2026-06-11 20:50:45 +0700

    delete

--- public/assets/js/filter.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const categoryFilter = document.getElementById('categoryFilter');
-
-    if (categoryFilter) {
-        categoryFilter.addEventListener('change', function () {
-            this.form.submit();
-        });
-    }
-});
